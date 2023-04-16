5fdca77d8a74f19864c659772bf870c2f9fab248
108432TorresMauricio <80530463+Mauriciocba@users.noreply.github.com>
2023-04-16 19:45:49 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       initialRoute: 'inicio',
       routes: {
         'inicio': (_) => const InicioScreen(),
-        'detalles': (context) => const DetallesScreen()
+        'detalles': (context) => const DetallesScreen()//
       },
     );
   }
